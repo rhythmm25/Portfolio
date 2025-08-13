aaca8a9c2c501c660ee5db642b45c703ce16b44a
rhythmm25 <rhythm.agg25@gmail.com>
2025-08-13 22:38:21 +0530

    commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -115,7 +115,7 @@ export default function Hero() {
 
         <a
           href="/Resume.pdf"
-          download="Rhythm_Aggarwal_Resume.pdf"
+          download="RhythmCv.pdf"
           className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-emerald-600 dark:bg-emerald-600 text-white hover:bg-emerald-700 dark:hover:bg-emerald-700 h-11 px-8 w-full sm:w-auto"
           aria-label="Download Resume"
         >
